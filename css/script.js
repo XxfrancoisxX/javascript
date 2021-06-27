48b0adca5646fa9f5cbcd27ac32d2292017958cd
Francois <haliowerlive.fr>
2021-06-27 16:31:08 +0200

    initial commit

---
+++ css/script.js
@@ -0,0 +1,61 @@
+
+//let nombre = prompt('entrez un nombre');
+
+// console.log(nombre);
+
+// function ajoutOnze(nombreRecu){
+
+//    console.log(parseInt(nombreRecu) + 11);
+// }
+
+// ajoutOnze(nombre);
+
+// function retireUn(nombreRecu){
+
+//    console.log(parseInt(nombreRecu) - 1);
+// }
+
+// retireUn(nombre);
+
+
+
+// function setfontsize(){
+
+
+// monElement = document.getElementById('monElement');
+// monElement.style.color = "blue"
+// }
+
+// var x = document.createElement("P");                        // Create a <p> node
+// var t = document.createTextNode("C'est un");  
+// x.appendChild(t);                                           // Append the text to <p>
+// document.body.appendChild(x);     //
+
+
+
+   //-------------------------
+   monElelementP = document.getElementById('evenement');
+   //---------
+monElelementP.addEventListener("mouseover",function()
+{
+//-------------------------
+maDiv = document.getElementById('monElement');
+//----------------------------
+maDiv.style.backgroundColor = "blue";
+
+});
+
+
+
+
+  //-------------------------
+  monElelementP = document.texte_exercice_un('evenement');
+  //---------
+monElelementP.addEventListener("mouseover",function()
+{
+//-------------------------
+maDiv = document.getElementById('texte_exercice_un');
+//----------------------------
+object.addEventListener("click", myScript);
+
+});
